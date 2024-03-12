1c0087d3fdcae5d7c558b3e03220fc212e47a277
Nikita <nikita.ushackow.02@gmail.com>
2024-03-12 23:05:04 +0200

    change some logic

---
+++ lab2/main3.py
@@ -0,0 +1,154 @@
+import numpy as np
+
+
+def format(number):
+    return "{:.25f}".format(float(number))
+
+
+def need_round(num):
+    return round(float(num), 15)
+
+
+A = [[5.9, 1.2, 2.1, 0.9],
+    [1.2, 7.2, 1.5, 2.5],
+    [2.1, 1.5, 9.8, 1.3],
+    [0.9, 2.5, 1.3, 6.1]]
+B = [-2, 5.3, 10.3, 12.6]
+
+A_origin = [row.copy() for row in A]
+B_origin = B.copy()
+
+
+def gauss_elimination(A, B):
+    n = len(A)
+    p = list(range(n))
+    for i in range(n - 1):
+        max_index = i
+        for j in range(i + 1, n):
+            if abs(A[j][i]) > abs(A[max_index][i]):
+                max_index = j
+        if max_index != i:
+            A[i], A[max_index] = A[max_index], A[i]
+            B[i], B[max_index] = B[max_index], B[i]
+            p[i], p[max_index] = p[max_index], p[i]
+        for j in range(i + 1, n):
+            factor = A[j][i] / A[i][i]
+            for k in range(i, n):
+                A[j][k] -= factor * A[i][k]
+            B[j] -= factor * B[i]
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            A[i][j] = need_round(A[i][j])
+    for i in range(len(B)):
+        B[i] = need_round(B[i])
+    return A, B, p
+
+
+def back_substitution(U, b):
+    n = len(b)
+    x = [0] * n
+    for i in range(n - 1, -1, -1):
+        summation = sum(U[i][j] * x[j] for j in range(i + 1, n))
+        x[i] = (b[i] - summation) / U[i][i]
+    return x
+
+
+print("Метод Гаусса")
+U, c, p = gauss_elimination(A, B)
+
+print("Треугольная матрица:")
+for row in U:
+    print(row)
+
+print("Решение СЛАУ:")
+x = back_substitution(U, c)
+print(x)
+
+
+diff = [sum(A_origin[i][j] * x[j] for j in range(len(x))) - B_origin[i] for i in range(len(x))]
+error = max(abs(val) for val in diff)
+print("Погрешность: ", format(error))
+
+
+def check_convergence(A):
+    n = len(A)
+    flag = True
+    for i in range(n):
+        summ = 0
+        for j in range(n):
+            if i != j:
+                summ += np.abs(A[i][j])
+        if not (np.abs(A[i][i]) >= summ):
+            print('расходится')
+            return False
+    print('Сходится')
+    return True
+
+
+def prepare_matrix(A, b):
+    n = len(b)
+    for i in range(n):
+        if A[i, i] == 0:
+            A[i, i] = 1e-10
+        for j in range(n):
+            if i != j:
+                multiplier = A[j, i] / A[i, i]
+                for k in range(n):
+                    A[j, k] -= multiplier * A[i, k]
+                b[j] -= multiplier * b[i]
+    return A, b
+
+
+def seidel_iteration(A, b, x):
+    n = len(b)
+    for i in range(n):
+        x_new = b[i]
+        for j in range(n):
+            if j != i:
+                x_new -= A[i, j] * x[j]
+        x_new /= A[i, i]
+        x = x.astype(np.float64)
+        x[i] = x_new
+    return x
+
+
+
+def seidel(A, b, x, tol=1e-10, max_iter=100):
+    print(A)
+    if not check_convergence(A):
+        return False, False
+    else:
+        iter = 0
+        for i in range(max_iter):
+            x_new = np.copy(x)
+            tmp = []
+            for j in range(4):
+                tmp.append(x[j])
+            print("Итерация: ", i+1)
+            print("Решение: ", tmp)
+            x = seidel_iteration(A, b, x)
+            print()
+            if (np.linalg.norm(x - x_new) < tol).all():
+                iter = i
+                break
+        return x, iter
+
+
+print("\nМетод Зейделя")
+x = np.zeros_like(B)
+diagonalized_A, diagonalized_b = prepare_matrix(np.array(A), np.array(B))
+solution, iterations = seidel(diagonalized_A, diagonalized_b, x)
+
+if not solution.all() and not iterations:
+    print("Решение расходится")
+else:
+    tmp = []
+    for i in range(4):
+        tmp.append(round(float(solution[i]), 20))
+
+    print("Количество итераций:", iterations + 1)
+    print("Решение:", tmp)
+
+    error = max(
+        abs(sum(A_origin[i][j] * solution[j] for j in range(len(solution))) - B_origin[i]) for i in range(len(B)))
+    print("Погрешность:", format(error))
--- lab3/main.py
+++ lab3/main.py
@@ -1,27 +1,30 @@
 import math
-from scipy import integrate
+
+import scipy.integrate as spi
 
 
 def long_format(number):
     return "{:.15f}".format(number)
 
 
-def short_format(number):
-    return "{:.6f}".format(number)
+def f(x):
+    return x * (x ** 2 - 1) ** 3 / 2
 
 
 def f(x):
     return 2 * x * math.exp(x ** 2)
 
 
+def F(x):
+    return math.exp(x ** 2)
+
 a = 0
 b = 2
-n = 2
 eps = 10**(-4)
 
 
-def left_rectangle(n):
-    prev_sum = 0
+def left_endpoint_rule():
+    n, prev_sum = 5, 0
     while True:
         h = ((b - a) / n)
         sum = 0
@@ -29,59 +32,59 @@ def left_rectangle(n):
             sum += f(a + h * i)
         sum *= h
         if abs(sum - prev_sum) < eps:
-            print(f"Последний: {short_format(h)}, точек разбиения = {n}")
+            print(f'Последний шаг = {long_format(h)}, количество точек = {n}')
             return sum
         prev_sum = sum
         n *= 2
 
 
-def right_rectangle(n):
-    prev_sum = 1
+def right_endpoint_rule():
+    n, prev_sum = 5, 0
     while True:
         h = ((b - a) / n)
         sum = 0
-        for i in range(n):
+        for i in range(1, n + 1):
             sum += f(a + h * i)
         sum *= h
         if abs(sum - prev_sum) < eps:
-            print(f"Последний: {short_format(h)}, точек разбиения = {n}")
+            print(f'Последний шаг = {long_format(h)}, количество точек = {n}')
             return sum
         prev_sum = sum
         n *= 2
 
 
-def middle_rectangle(n):
-    prev_sum = 0
+def middle_endpoint_rule():
+    n, prev_sum = 5, 0
     while True:
         h = ((b - a) / n)
         sum = f(a + (h / 2))
-        for i in range(1, n + 1):
+        for i in range(1, n):
             sum += f(a + (h / 2) + (h * i))
         sum *= h
         if abs(sum - prev_sum) < eps:
-            print(f"Последний: {short_format(h)}, точек разбиения = {n}")
+            print(f'Последний шаг = {long_format(h)}, количество точек = {n}')
             return sum
         prev_sum = sum
         n *= 2
 
 
-def trapezoid_method(n):
-    prev_sum = 0
+def trapezoidal_rule():
+    n, prev_sum = 5, 0
     while True:
         h = ((b - a) / n)
         sub_sum = 0
-        for i in range(1, n + 1):
+        for i in range(n):
             sub_sum += f(a + h * i)
         sum = (h * (f(a) + f(b)) / 2) + h * sub_sum
         if abs(sum - prev_sum) < eps:
-            print(f"Последний: {short_format(h)}, точек разбиения = {n}")
+            print(f'Последний шаг = {long_format(h)}, количество точек = {n}')
             return sum
         prev_sum = sum
         n *= 2
 
 
-def simpson_method(n):
-    prev_sum = 0
+def simpsons_rule():
+    n, prev_sum = 5, 0
     while True:
         h = ((b - a) / n)
         sub_sum_even, sub_sum_odd = 0, 0
@@ -90,44 +93,45 @@ def simpson_method(n):
                 sub_sum_even += f(a + h * i)
         for i in range(1, n + 1, 2):
             sub_sum_odd += f(a + h * i)
-        sum = (h * (f(a) + f(b) + 2 * sub_sum_even + 4 * sub_sum_odd) / 3)
+        sum = h * (f(a) + f(b) + 2 * sub_sum_even + 4 * sub_sum_odd) / 3
         if abs(sum - prev_sum) < eps:
-            print(f"Последний шаг = {short_format(h)}, кол-во точек разбиения = {n}")
+            print(f'Последний шаг = {long_format(h)}, количество точек = {n}')
             return sum
         prev_sum = sum
         n *= 2
 
 
-result, error = integrate.quad(f, a, b)
-print(f"Точное решение: {result}")
+def error_rate(res, exact):
+    return abs(1 - res / exact) * 100
+
+
+result, er = spi.quad(f, a, b)
+
+res_1 = left_endpoint_rule()
+err_1 = error_rate(res_1, result)
+print(f'Приближенное решение методом левых прямоугольников: {long_format(res_1)}')
+print(f'Относительная погрешность: {long_format(err_1)}')
 print()
 
-res_1 = left_rectangle(n)
-error_rate_1 = (abs(result - res_1) / result) * 100
-print(f"Приближенное решение метода левых прямоугольников: {res_1}")
-print(f"Относительная погрешность: {short_format(error_rate_1)}")
+res_2 = right_endpoint_rule()
+err_2 = error_rate(res_2, result)
+print(f'Приближенное решение методом правых прямоугольников: {long_format(res_2)}')
+print(f'Относительная погрешность: {long_format(err_2)}')
 print()
 
-res_2 = right_rectangle(n)
-error_rate_2 = (abs(result - res_2) / result) * 100
-print(f"Приближенное решение метода правых прямоугольников: {res_2}")
-print(f"Относительная погрешность: {short_format(error_rate_2)}")
+res_3 = middle_endpoint_rule()
+err_3 = error_rate(res_3, result)
+print(f'Приближенное решение методом средних прямоугольников: {long_format(res_3)}')
+print(f'Относительная погрешность: {long_format(err_3)}')
 print()
 
-res_3 = middle_rectangle(n)
-error_rate_3 = (abs(result - res_3) / result) * 100
-print(f"Приближенное решение метода средних прямоугольников: {res_3}")
-print(f"Относительная погрешность: {short_format(error_rate_3)}")
+res_4 = trapezoidal_rule()
+err_4 = error_rate(res_4, result)
+print(f'Приближенное решение методом трапеций: {long_format(res_4)}')
+print(f'Относительная погрешность: {long_format(err_4)}')
 print()
 
-res_4 = trapezoid_method(n)
-error_rate_4 = (abs(result - res_4) / result) * 100
-print(f"Приближенное решение метода трапеций: {res_4}")
-print(f"Относительная погрешность: {short_format(error_rate_4)}")
-print()
-
-res_5 = simpson_method(n)
-error_rate_5 = (abs(result - res_5) / result) * 100
-print(f"Приближенное решение метода Симсона: {res_5}")
-print(f"Относительная погрешность: {short_format(error_rate_5)}")
-print()
+res_5 = simpsons_rule()
+err_5 = error_rate(res_5, result)
+print(f'Приближенное решение методом Симпсона: {long_format(res_5)}')
+print(f'Относительная погрешность: {long_format(err_5)}')

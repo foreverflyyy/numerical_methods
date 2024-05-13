f5a94fb75408bb5ddbf4626fecccf591d8c9a656
Nikita <nikita.ushackow.02@gmail.com>
2024-05-13 13:18:47 +0200

    done 9 and 10 labs

--- lab10/main.py
+++ lab10/main.py
@@ -1,1 +1,98 @@
-print("hi")
+import math as m
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+
+eps = 0.01
+a, b = c, d = 0, 10
+
+
+def f(x, y):
+    return 2 * x * y
+
+
+def simple_iteration_method(x, N, h):
+    X = np.copy(x)
+    for i in range(1, N - 1):
+        for j in range(1, N - 1):
+            X[i][j] = 1 / 4 * (x[i][j - 1] + x[i][j + 1] + x[i - 1][j] + x[i + 1][j] + f(i * h, j * h) * (h ** 2))
+    return X
+
+
+def simple_diff(x, y):
+    result = 0
+    lenght = len(x)
+    for i in range(lenght):
+        for j in range(len(x[i])):
+            result += (x[i][j] - y[i][j]) ** 2
+        return m.sqrt(result)
+
+
+def seidel_method(x, N, h):
+    X = np.copy(x)
+    for i in range(1, N - 1):
+        for j in range(1, N - 1):
+            X[i][j] = 1 / 4 * (X[i][j - 1] + x[i][j + 1] + x[i - 1][j] + x[i + 1][j] + f(i * h, j * h) * (h ** 2))
+    return X
+
+
+def seidel_diff(x, y):
+    result = 0
+    lenght = len(x)
+    for i in range(lenght):
+        for j in range(len(x[i])):
+            result = max(abs(x[i][j] - y[i][j]), result)
+    return result
+
+
+def draw(a, b, c, d, h, u, method):
+    x = np.arange(a, b, h)
+    t = np.arange(c, d, h)
+    x, t = np.meshgrid(x, t)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(x, t, u, cmap=cm.summer)
+    ax.set_title(method.title())
+    plt.show()
+
+
+def get_method(a, b, c, d, h, method):
+    count_x = int((b - a) / h)
+    count_t = int((d - c) / h)
+    u = np.zeros((count_t, count_x))
+    for i in range(count_t):
+        u[i][0] = i * h + a
+        u[i][count_x - 1] = i * h + b
+    for i in range(count_x):
+        u[0][i] = i * h + c
+        u[count_t - 1][i] = i * h + d
+
+    for j in range(1, count_x - 1):
+        for i in range(1, count_t - 1):
+            u[i][j] = 0
+    prev = next = u
+    while True:
+        prev = next
+        if method.title() == 'SeidelMethod':
+            next = seidel_method(prev, count_x, h)
+            if seidel_diff(prev, next) * h < eps:
+                break
+        else:
+            next = simple_iteration_method(prev, count_x, h)
+            if simple_diff(prev, next) * h < eps:
+                break
+    return next
+
+
+data = [
+    {"a": 0, "b": 10, "c": 0, "d": 10, "h": 1, "method": 'Зейдель, 10 * 10'},
+    {"a": 0, "b": 5, "c": 0, "d": 5, "h": 1, "method": 'Зейдель, 5 * 5'},
+    {"a": 0, "b": 10, "c": 0, "d": 10, "h": 1, "method": 'Простые итерации, 10 * 10'},
+    {"a": 0, "b": 5, "c": 0, "d": 5, "h": 1, "method": 'Простые итерации, 5 * 5'}
+]
+
+for cell in data:
+    a, b, c, d, h, method = cell["a"], cell["b"], cell["c"], cell["d"], cell["h"], cell["method"]
+    u = get_method(a, b, c, d, h, method)
+    draw(a, b, c, d, h, u, method)
--- lab9/main.py
+++ lab9/main.py
@@ -1,1 +1,65 @@
-print("hi")
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+a, b, h = 0, 1, 0.1
+c, d, tau = 0, 10, 0.005
+I, J = int((b - a) / h), int((d - c) / tau)
+lambd = tau / (h ** 2)
+
+
+def U_0x(x):
+    return 1 + x - x * (1 - x)
+
+
+def U_t0(t):
+    return 1
+
+
+def U_t1(t):
+    return 2
+
+
+def conditions():
+    u = np.zeros((J, I))
+    for i in range(I):
+        u[0][i] = U_0x(i * h)
+    for i in range(J):
+        u[i][0] = U_t0(i * tau)
+        u[i][I - 1] = U_t1(i * tau)
+    return u
+
+
+def explicit_scheme():
+    u = conditions()
+    for j in range(J - 1):
+        for i in range(1, I - 1):
+            u[j + 1][i] = lambd * u[j][i + 1] + (1 - 2 * lambd) * u[j][i] + lambd * u[j][i - 1]
+    return u
+
+
+def not_explicit_scheme():
+    u = conditions()
+    alpha, beta, gamma, A, B, C = np.zeros(I), np.zeros(I), np.zeros(I), lambd, 2 * lambd + 1, lambd
+    for j in range(J - 1):
+        alpha[I - 1] = 0
+        beta[I - 1] = u[j + 1][I - 1]
+        gamma[I - 1] = 1 / (B - C * alpha[I - 1])
+        for i in range(I - 1, 0, -1):
+            alpha[i - 1] = gamma[i] * A
+            beta[i - 1] = gamma[i] * (C * beta[i] + u[j][i])
+            gamma[i - 1] = 1 / (B - C * alpha[i - 1])
+        for i in range(I - 1):
+            u[j + 1][i + 1] = alpha[i] * u[j + 1][i] + beta[i]
+    return u
+
+
+data = [(explicit_scheme(), 'Явная схема'), (not_explicit_scheme(), 'Неявная схема')]
+for (graph, title) in data:
+    x, t = np.meshgrid(np.arange(a, b, h), np.arange(c, d, tau))
+    fig = plt.figure(title)
+    ax = fig.add_subplot(projection='3d')
+    ax.view_init(30, -120)
+    ax.plot_surface(x, t, graph, cmap=cm.summer, antialiased=False)
+
+plt.show()
